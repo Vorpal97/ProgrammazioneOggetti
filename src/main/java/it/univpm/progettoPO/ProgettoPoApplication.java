e78c79b3de1933ff9f20aab57c1b4fc863da79b2
Manuel Manelli <manuelmanelli@icloud.com>
2019-07-08 16:47:38 +0200

    Initial commit

---
+++ src/main/java/it/univpm/progettoPO/ProgettoPoApplication.java
@@ -0,0 +1,13 @@
+package it.univpm.progettoPO;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProgettoPoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProgettoPoApplication.class, args);
+	}
+
+}
